0ec47d9ae4076203ea69df55cf6bcae3b25176b3
Yael Saul Martell Romero <al356084@edu.uaa.mx>
2026-09-01 07:22:13 -0600

    Forms2

--- PiedraPapelTijera/PiedraPapelTijera/Program.cs
+++ PiedraPapelTijera/PiedraPapelTijera/Program.cs
@@ -16,7 +16,7 @@ namespace PiedraPapelTijera
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
