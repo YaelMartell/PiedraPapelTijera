b73f1f677a013bd18a39ccf36fdcf8c4970a7e35
Yael Saul Martell Romero <al356084@edu.uaa.mx>
2026-08-31 18:11:41 -0600

    Botones, imagenes, guardado de selecciones del usuario y total de juegos

--- PiedraPapelTijera/PiedraPapelTijera/Form1.cs
+++ PiedraPapelTijera/PiedraPapelTijera/Form1.cs
@@ -16,5 +16,49 @@ namespace PiedraPapelTijera
         {
             InitializeComponent();
         }
+
+        int op = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.Captura_de_pantalla_2026_08_31_083122;
+            op = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.Captura_de_pantalla_2026_08_31_083103;
+            op = 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.Captura_de_pantalla_2026_08_31_083117;
+            op = 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (op == 1)
+            {
+                Properties.Settings.Default.contPi++;
+                Properties.Settings.Default.Save();
+            }
+            else if (op == 2)
+            {
+                Properties.Settings.Default.contPa++;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Properties.Settings.Default.contTi++;
+                Properties.Settings.Default.Save();
+            }
+
+            Properties.Settings.Default.totDat++;
+            Properties.Settings.Default.Save();
+
+
+        }
     }
 }
